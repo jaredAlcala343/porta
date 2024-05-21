3c1ee03390a855d8e82fe5c2691d685dd61353a3
JARED ALCALA <jalcalaing@gmail.com>
2024-05-21 04:55:45 -0600

    correcciones
    correcciones

--- portafolio/src/components/navbar.js
+++ portafolio/src/components/navbar.js
@@ -1,15 +1,10 @@
 // src/components/Navbar.js
 import React from 'react';
-import { useNavbar } from './NavbarContext';
+
 import './navbar.css';
 import porfile from '../foto2.jpg'
 
 const Navbar = () => {
-  const { setActiveComponent } = useNavbar();
-
-  const handleItemClick = (component) => {
-    setActiveComponent(component);
-  };
 
   return (
     <nav className="navbar">
@@ -19,7 +14,7 @@ const Navbar = () => {
         <h2>Ingeniero en Sistemas</h2>
       </div>
       <ul>
-        <li onClick={() => handleItemClick('About')}>About</li>
+        <li href="#About">About</li>
         <li onClick={() => handleItemClick('ExperienciaProfesional')}>Experiencia Profesional</li>
         <li onClick={() => handleItemClick('Projects')}>Projects</li>
         <li onClick={() => handleItemClick('Gallery')}>Gallery</li>
--- portafolio/src/index.js
+++ portafolio/src/index.js
@@ -3,16 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { NavbarProvider } from './components/NavbarContext';
+
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <NavbarProvider>
       <App />
-    </NavbarProvider>
   </React.StrictMode>
 );
 
